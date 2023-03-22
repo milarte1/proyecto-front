2d0accab30685b1408e8f25b48cde21c432e8826
milarte1 <116848503+milarte1@users.noreply.github.com>
2023-03-22 16:44:27 +0100

    Automotive

---
+++ main.js
@@ -0,0 +1,29 @@
+// Define the images
+const images =[
+
+    "img/backgroun_descarga_(1).jpg";
+    "img/backgroun_descarga_(2).jpg";
+    "img/backgroun_descarga_.jpg";
+]
+//app store
+let index = 0;
+const max = images.length;
+
+// Get the DOM elements
+const containerElement = document.querySlesector(".container")
+const prevslideButton = document.querySlesector(".prevslide")
+const nexvslideButton = document.querySlesector(".nexvslide")
+
+
+// Listen for arrow click events
+prevslideButton.addEventsListener("click",function(){
+index--
+//Set Image Index
+/ Charge Background Imgage
+
+})
+
+// Utility function
+function setImageIndex() {
+    if (index 
+}
